3741d959249065ddf9f8014fbd8205ffc7f81626
Jst77 <justinte7@gmail.com>
2022-05-05 10:30:25 -0500

    updated javascript

---
+++ app.js
@@ -0,0 +1,23 @@
+function computerPlay() { 
+    let compOptions = ["rock", "paper", "sciscors"]
+    var thing = compOptions[Math.floor(Math.random()*compOptions.length)];
+    
+
+    if (thing === "rock") { 
+        alert("rock!") 
+    } else { 
+        alert("hello")
+    }
+
+
+
+    
+    
+}
+
+computerPlay();
+
+
+
+
+
